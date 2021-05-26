0169f6b8d827aac415d181827beb6d9f610a0e3b
DamianoD88 <dispe88@live.it>
2021-05-26 14:51:56 +0200

    add java init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+var nome = prompt('inserisci Nome');
+console.log(nome);
 
+var cognome = prompt('inserisci Cognome');
+console.log(cognome);
+
+var colore = prompt('inserisci Colore Preferito');
+console.log(colore);
+
+var somma = nome + cognome + colore + '21';
+document.getElementById('parole').innerHTML = somma;
+console.log(somma);
